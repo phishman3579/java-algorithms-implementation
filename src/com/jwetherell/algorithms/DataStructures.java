6e88f2511a1647bdc97badbc22338a8f5f2baa1f
phishman3579 <phishman3579@032fbc0f-8cab-eb90-e552-f08422b9a96a>
2012-04-11 19:11:20 +0000

    Refactored some testing code
    git-svn-id: https://java-algorithms-implementation.googlecode.com/svn/trunk@170 032fbc0f-8cab-eb90-e552-f08422b9a96a

--- src/com/jwetherell/algorithms/DataStructures.java
+++ src/com/jwetherell/algorithms/DataStructures.java
@@ -40,7 +40,7 @@ public class DataStructures {
 
     private static final int NUMBER_OF_TESTS = 100;
     private static final Random RANDOM = new Random();
-    private static final int ARRAY_SIZE = 1000;
+    private static final int ARRAY_SIZE = 100;
 
     private static Integer[] unsorted = null;
     private static String string = null;
