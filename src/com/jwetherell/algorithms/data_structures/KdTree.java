94f9cb48aa8bf5ac5e07b06b252899b5a8226132
phishman3579 <phishman3579@032fbc0f-8cab-eb90-e552-f08422b9a96a>
2012-06-27 20:01:04 +0000

    Renamed some variables to be more explicit in the KD-Tree
    git-svn-id: https://java-algorithms-implementation.googlecode.com/svn/trunk@369 032fbc0f-8cab-eb90-e552-f08422b9a96a

--- src/com/jwetherell/algorithms/data_structures/KdTree.java
+++ src/com/jwetherell/algorithms/data_structures/KdTree.java
@@ -353,46 +353,42 @@ public class KdTree<T extends KdTree.XYZPoint> {
         if (lesser!=null && !examined.contains(lesser)) {
             examined.add(lesser);
 
-            double p1 = Double.MIN_VALUE;
-            double p2 = Double.MIN_VALUE;
+            double nodePoint = Double.MIN_VALUE;
+            double valuePlusDistance = Double.MIN_VALUE;
             if (axis==X_AXIS) {
-                p1 = node.id.x;
-                p2 = value.x-lastDistance;
+                nodePoint = node.id.x;
+                valuePlusDistance = value.x-lastDistance;
             } else if (axis==Y_AXIS) {
-                p1 = node.id.y;
-                p2 = value.y-lastDistance;
+                nodePoint = node.id.y;
+                valuePlusDistance = value.y-lastDistance;
             } else {
-                p1 = node.id.z;
-                p2 = value.z-lastDistance;
+                nodePoint = node.id.z;
+                valuePlusDistance = value.z-lastDistance;
             }
-            boolean lineIntersectsCube = ((p2<=p1)?true:false);
+            boolean lineIntersectsCube = ((valuePlusDistance<=nodePoint)?true:false);
 
             //Continue down lesser branch
-            if (lineIntersectsCube) {
-                searchNode(value,lesser,K,results,examined);
-            }
+            if (lineIntersectsCube) searchNode(value,lesser,K,results,examined);
         }
         if (greater!=null && !examined.contains(greater)) {
             examined.add(greater);
 
-            double p1 = Double.MIN_VALUE;
-            double p2 = Double.MIN_VALUE;
+            double nodePoint = Double.MIN_VALUE;
+            double valuePlusDistance = Double.MIN_VALUE;
             if (axis==X_AXIS) {
-                p1 = node.id.x;
-                p2 = value.x+lastDistance;
+                nodePoint = node.id.x;
+                valuePlusDistance = value.x+lastDistance;
             } else if (axis==Y_AXIS) {
-                p1 = node.id.y;
-                p2 = value.y+lastDistance;
+                nodePoint = node.id.y;
+                valuePlusDistance = value.y+lastDistance;
             } else {
-                p1 = node.id.z;
-                p2 = value.z+lastDistance;
+                nodePoint = node.id.z;
+                valuePlusDistance = value.z+lastDistance;
             }
-            boolean lineIntersectsCube = ((p2>=p1)?true:false);
+            boolean lineIntersectsCube = ((valuePlusDistance>=nodePoint)?true:false);
 
             //Continue down greater branch
-            if (lineIntersectsCube) {
-                searchNode(value,greater,K,results,examined);
-            }
+            if (lineIntersectsCube) searchNode(value,greater,K,results,examined);
         }
     }
 
