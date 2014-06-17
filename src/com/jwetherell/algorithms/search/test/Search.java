561a254b87590a9a89f1ad10aaa6800e0868dadf
Justin Wetherell <justin.wetherell@nasdaqomx.com>
2014-06-17 11:53:27 -0400

    Changed the Search tests to use JUnit

--- src/com/jwetherell/algorithms/search/test/Search.java
+++ src/com/jwetherell/algorithms/search/test/Search.java
@@ -1,100 +1,66 @@
 package com.jwetherell.algorithms.search.test;
 
-import java.text.DecimalFormat;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
 
 import com.jwetherell.algorithms.search.BinarySearch;
-import com.jwetherell.algorithms.search.LinearSearch;
 import com.jwetherell.algorithms.search.InterpolationSearch;
+import com.jwetherell.algorithms.search.LinearSearch;
 import com.jwetherell.algorithms.search.QuickSelect;
 
 public class Search {
 
-    private static final DecimalFormat FORMAT = new DecimalFormat("#.######");
-    private static final int SIZE = 9999;
-    private static final int offset = 123;
+    private static final int           SIZE            = 9999;
+    private static final int           offset          = 123;
 
-    private static int[] sorted = null;
-
-    public static void main(String[] args) {
-        System.out.println("Generating sorted array.");
-        sorted = new int[SIZE];
-        for (int i = offset; i < offset + sorted.length; i++) {
-            sorted[i - offset] = i;
+    private static int[]               sorted          = new int[SIZE];
+    static {
+        for (int i = offset; i<offset+sorted.length; i++) {
+            sorted[i-offset] = i;
         }
-        System.out.println("Generated sorted array.");
-        System.out.println();
+    }
+    private static int                 valueIndex      = SIZE-(SIZE/4);
+    private static int                 valueInArray    = sorted[valueIndex];
+    private static int                 valueNotInArray = sorted[SIZE-1]+offset;
 
-        int valueInArray = sorted[SIZE - (SIZE / 4)];
-        int valueNotInArray = sorted[SIZE - 1] + offset;
+    @Test
+    public void testBruteForceSearch() {
+        int index = LinearSearch.find(valueInArray, sorted);
+        assertTrue("Brute force error. expected="+valueIndex+" got="+index, (index==valueIndex));
+        index = LinearSearch.find(valueNotInArray, sorted);
+        assertTrue("Brute force error. expected="+Integer.MAX_VALUE+" got="+index, (index==Integer.MAX_VALUE));
+    }
 
-        {
-            System.out.println("Brute Force.");
-            long before = System.nanoTime();
-            LinearSearch.find(valueInArray, sorted);
-            long after = System.nanoTime();
-            System.out.println("Computed in " + FORMAT.format(after - before) + " ns");
-            before = System.nanoTime();
-            LinearSearch.find(valueNotInArray, sorted);
-            after = System.nanoTime();
-            System.out.println("Computed in " + FORMAT.format(after - before) + " ns");
-            System.out.println();
-            System.gc();
-        }
+    @Test
+    public void testBinarySearch() {
+        int index = BinarySearch.find(valueInArray, sorted, false);
+        assertTrue("Brute force error. expected="+valueIndex+" got="+index, (index==valueIndex));
+        index = BinarySearch.find(valueNotInArray, sorted, false);
+        assertTrue("Brute force error. expected="+Integer.MAX_VALUE+" got="+index, (index==Integer.MAX_VALUE));
+    }
 
-        {
-            System.out.println("Binary Search.");
-            long before = System.nanoTime();
-            BinarySearch.find(valueInArray, sorted, false);
-            long after = System.nanoTime();
-            System.out.println("Computed in " + FORMAT.format(after - before) + " ns");
-            before = System.nanoTime();
-            BinarySearch.find(valueNotInArray, sorted, false);
-            after = System.nanoTime();
-            System.out.println("Computed in " + FORMAT.format(after - before) + " ns");
-            System.out.println();
-            System.gc();
-        }
+    @Test
+    public void testOptimizedBinarySearch() {
+        int index = BinarySearch.find(valueInArray, sorted, true);
+        assertTrue("Brute force error. expected="+valueIndex+" got="+index, (index==valueIndex));
+        index = BinarySearch.find(valueNotInArray, sorted, true);
+        assertTrue("Brute force error. expected="+Integer.MAX_VALUE+" got="+index, (index==Integer.MAX_VALUE));
+    }
 
-        {
-            System.out.println("Optimized Binary Search.");
-            long before = System.nanoTime();
-            BinarySearch.find(valueInArray, sorted, true);
-            long after = System.nanoTime();
-            System.out.println("Computed in " + FORMAT.format(after - before) + " ns");
-            before = System.nanoTime();
-            BinarySearch.find(valueNotInArray, sorted, true);
-            after = System.nanoTime();
-            System.out.println("Computed in " + FORMAT.format(after - before) + " ns");
-            System.out.println();
-            System.gc();
-        }
+    @Test
+    public void testInterpolationSearch() {
+        int index = InterpolationSearch.find(valueInArray, sorted);
+        assertTrue("Brute force error. expected="+valueIndex+" got="+index, (index==valueIndex));
+        index = InterpolationSearch.find(valueNotInArray, sorted);
+        assertTrue("Brute force error. expected="+Integer.MAX_VALUE+" got="+index, (index==Integer.MAX_VALUE));
+    }
 
-        {
-            System.out.println("Interpolation Search.");
-            long before = System.nanoTime();
-            InterpolationSearch.find(valueInArray, sorted);
-            long after = System.nanoTime();
-            System.out.println("Computed in " + FORMAT.format(after - before) + " ns");
-            before = System.nanoTime();
-            InterpolationSearch.find(valueNotInArray, sorted);
-            after = System.nanoTime();
-            System.out.println("Computed in " + FORMAT.format(after - before) + " ns");
-            System.out.println();
-            System.gc();
-        }
-
-        {
-            System.out.println("Quick Select");
-            long before = System.nanoTime();
-            QuickSelect.find(valueInArray, sorted);
-            long after = System.nanoTime();
-            System.out.println("Computed in " + FORMAT.format(after - before) + " ns");
-            before = System.nanoTime();
-            QuickSelect.find(valueNotInArray, sorted);
-            after = System.nanoTime();
-            System.out.println("Computed in " + FORMAT.format(after - before) + " ns");
-            System.out.println();
-            System.gc();
-        }
+    @Test
+    public void testQuickSelect() {
+        int index = QuickSelect.find(valueInArray, sorted);
+        assertTrue("Brute force error. expected="+valueIndex+" got="+index, (index==valueIndex));
+        index = QuickSelect.find(valueNotInArray, sorted);
+        assertTrue("Brute force error. expected="+Integer.MAX_VALUE+" got="+index, (index==Integer.MAX_VALUE));
     }
 }
