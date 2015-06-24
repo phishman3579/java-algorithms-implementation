48fe8efe124c3facf30175a130b78f66a72ad942
Justin Wetherell <justin.wetherell@nasdaq.com>
2015-06-24 16:52:00 -0400

    Fixed a small mis-type in sort timing code

--- src/com/jwetherell/algorithms/sorts/timing/SortsTiming.java
+++ src/com/jwetherell/algorithms/sorts/timing/SortsTiming.java
@@ -130,7 +130,7 @@ public class SortsTiming {
         putOutTheGarbage();
 
         bInsertion = System.nanoTime();
-        result = InsertionSort.sort(sorted.clone());
+        result = testable.sort(sorted.clone());
         if (checkResults && !check(result))
             System.err.println(testable.getName()+" failed.");
         aInsertion = System.nanoTime();
@@ -142,9 +142,9 @@ public class SortsTiming {
         putOutTheGarbage();
 
         bInsertion = System.nanoTime();
-        result = InsertionSort.sort(reverse.clone());
+        result = testable.sort(reverse.clone());
         if (checkResults && !check(result))
-            System.err.println(testable.getName()+"InsertionSort.");
+            System.err.println(testable.getName()+" failed.");
         aInsertion = System.nanoTime();
         diff = (aInsertion - bInsertion) / 1000000d / 1000d;
         System.out.println("Reverse sorted: "+testable.getName()+"=" + FORMAT.format(diff) + " secs");
