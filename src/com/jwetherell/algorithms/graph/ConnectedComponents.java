f2c5d1a4cd0dac4546d2222395e623c5d4c7a54c
Justin Wetherell <phishman3579@gmail.com>
2016-01-05 16:35:11 -0500

    Changed links from https to http

--- src/com/jwetherell/algorithms/graph/ConnectedComponents.java
+++ src/com/jwetherell/algorithms/graph/ConnectedComponents.java
@@ -14,7 +14,7 @@ import com.jwetherell.algorithms.data_structures.Graph.Vertex;
  * other by paths, and which is connected to no additional vertices in the supergraph. A vertex with no incident edges is itself a connected 
  * component. A graph that is itself connected has exactly one connected component, consisting of the whole graph.
  * 
- * https://en.wikipedia.org/wiki/Connected_component_(graph_theory)
+ * http://en.wikipedia.org/wiki/Connected_component_(graph_theory)
  * 
  * @author Justin Wetherell <phishman3579@gmail.com>
  */
