f2b9a21083f3aaf78bff990cd63a3049673e4be2
Justin Wetherell <phishman3579@gmail.com>
2017-02-21 17:02:06 -0500

    Changed the KMP algorithm to be it's own class

--- src/com/jwetherell/algorithms/strings/KnuthMorrisPratt.java
+++ src/com/jwetherell/algorithms/strings/KnuthMorrisPratt.java
@@ -4,45 +4,54 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * This class contains methods for getting informations about text.
- *
+ * This class implements KMP algorithm for finding length of maximal prefix-suffix for each prefix of the string.
+ * Prefix-suffix of string S is a substring which occurs at the beginning and at the end of S.
+ * <p>
+ * Time complexity: O(n) <br>
+ * <br>
+ * https://en.wikipedia.org/wiki/Knuth%E2%80%93Morris%E2%80%93Pratt_algorithm
+ * <br>
  * @author Szymon Stankiewicz <mail@stankiewicz.me>
  * @author Justin Wetherell <phishman3579@gmail.com>
  */
-public class StringInformations {
+public class KnuthMorrisPratt {
+
+    private KnuthMorrisPratt() {}
 
     /**
      * This function implements KMP algorithm for finding length of maximal prefix-suffix for each prefix of the string.
      * Prefix-suffix of string S is a substring which occurs at the beginning and at the end of S.
-     *
-     * Time complexity: O(n)
-     *
+     * <p>
+     * Time complexity: O(n)<p>
+     * <p>
      * https://en.wikipedia.org/wiki/Knuth%E2%80%93Morris%E2%80%93Pratt_algorithm
-     *
+     * <br>
      * @param text Text
      * @return maximal length of prefix-suffix for each prefix of the string text
      */
     public static List<Integer> getPrefSufTable(String text) {
         final List<Integer> prefSufTable = new ArrayList<Integer>();
+        final char[] chars = text.toCharArray();
 
-        if(text.length() == 0) 
+        if (text.length() == 0) 
             return prefSufTable;
 
         prefSufTable.add(0);
 
-        for(int i = 1; i<text.length(); i++) {
+        for (int i = 1; i<chars.length; i++) {
             int sizeOfPrefSuf = prefSufTable.get(i-1);
-            while(sizeOfPrefSuf > 0 && text.charAt(i) != text.charAt(sizeOfPrefSuf)) {
+            while (sizeOfPrefSuf > 0 && (chars[i] != chars[sizeOfPrefSuf]))
                 sizeOfPrefSuf = prefSufTable.get(sizeOfPrefSuf-1); // because string is 0-indexed
-            }
+            System.out.println("a="+chars[i]+" b="+chars[sizeOfPrefSuf]);
 
             // if characters at this positions are different then sizeOfPrefSuf is equal to zero,
             // so there is no proper prefix-suffix
-            if(text.charAt(i) == text.charAt(sizeOfPrefSuf)) {
+            if (chars[i] == chars[sizeOfPrefSuf]) {
                 prefSufTable.add(sizeOfPrefSuf+1);
             } else {
                 prefSufTable.add(0);
             }
+            System.out.println("prefSufTable.size="+prefSufTable.size());
         }
         return prefSufTable;
     }
---
+++ test/com/jwetherell/algorithms/strings/test/KnuthMorrisPrattTests.java
@@ -0,0 +1,39 @@
+package com.jwetherell.algorithms.strings.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.jwetherell.algorithms.strings.KnuthMorrisPratt;
+
+public class KnuthMorrisPrattTests {
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void getPrefixSuffixes() throws Exception {
+        final List<Object[]> data = Arrays.asList(
+            new Object[][]{
+                {"", Arrays.asList()},
+                {"a", Arrays.asList(0)},
+                {"aaa", Arrays.asList(0, 1, 2)},
+                {"abbabb", Arrays.asList(0, 0, 0, 1, 2, 3)},
+                {"bbabbbbaab", Arrays.asList(0, 1, 0, 1, 2, 2, 2, 3, 0, 1)},
+                {
+                    "( ͡° ͜ʖ ͡° )( ͡° a( ͡° ͜ʖ ͡°",//"( ͡° ͜ʖ ͡° )( ͡° a( ͡° ͜ʖ ͡°",
+                    Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+                }
+            }
+        );
+        for (Object[] testCase: data) {
+            String input = (String) testCase[0];
+            List<Integer> expected = (List<Integer>) testCase[1];
+            List<Integer> result = KnuthMorrisPratt.getPrefSufTable(input);
+            System.out.println("expected="+expected+" result="+result);
+            assertEquals(expected, result);
+            result.clear();
+        }
+    }
+}
