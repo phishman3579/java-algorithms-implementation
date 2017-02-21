ad7c7aa9d5698685671342b50e8fdee0885aa546
mciancia <zasilacz>
2017-02-21 19:30:39 +0100

    Refactor

--- src/com/jwetherell/algorithms/numbers/Complex.java
+++ src/com/jwetherell/algorithms/numbers/Complex.java
@@ -6,39 +6,47 @@ package com.jwetherell.algorithms.numbers;
 public class Complex {
     public double real;
     public double imaginary;
-    public Complex(){
+
+    public Complex() {
         this.real = 0.0;
         this.imaginary = 0.0;
     }
-    public Complex(double r, double i ){
+
+    public Complex(double r, double i) {
         this.real = r;
         this.imaginary = i;
     }
-    public Complex multiply(final Complex x){
+
+    public Complex multiply(final Complex x) {
         Complex copy = new Complex(this.real, this.imaginary);
-        copy.real = this.real*x.real - this.imaginary*x.imaginary;
-        copy.imaginary = this.imaginary*x.real + this.real*x.imaginary;
+        copy.real = this.real * x.real - this.imaginary * x.imaginary;
+        copy.imaginary = this.imaginary * x.real + this.real * x.imaginary;
         return copy;
     }
-    public Complex add(final Complex x){
+
+    public Complex add(final Complex x) {
         Complex copy = new Complex(this.real, this.imaginary);
-        copy.real+= x.real;
-        copy.imaginary+= x.imaginary;
+        copy.real += x.real;
+        copy.imaginary += x.imaginary;
         return copy;
     }
-    public Complex sub(final Complex x){
+
+    public Complex sub(final Complex x) {
         Complex copy = new Complex(this.real, this.imaginary);
-        copy.real-= x.real;
-        copy.imaginary-= x.imaginary;
+        copy.real -= x.real;
+        copy.imaginary -= x.imaginary;
         return copy;
     }
-    public double abs(){
-        return Math.sqrt(this.real*this.real + this.imaginary*this.imaginary);
+
+    public double abs() {
+        return Math.sqrt(this.real * this.real + this.imaginary * this.imaginary);
     }
-    public String toString(){
+
+    public String toString() {
         return "(" + this.real + "," + this.imaginary + ")";
     }
-    public static Complex polar(final double rho, final double theta){
-        return new Complex(rho*Math.cos(theta), rho*Math.sin(theta));
+
+    public static Complex polar(final double rho, final double theta) {
+        return new Complex(rho * Math.cos(theta), rho * Math.sin(theta));
     }
 }
