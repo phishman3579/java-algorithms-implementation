14aea2e29229ccc03be0e7d64df795b4db658271
Justin Wetherell <phishman3579@gmail.com>
2017-02-22 14:05:04 -0500

    Combined the two GCD calculators into a single class and cleaned up sme code headers

--- test/com/jwetherell/algorithms/mathematics/test/GCD.java
+++ test/com/jwetherell/algorithms/mathematics/test/GCD.java
@@ -1,17 +1,41 @@
 package com.jwetherell.algorithms.mathematics.test;
 
-import com.jwetherell.algorithms.mathematics.Utils;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import com.jwetherell.algorithms.mathematics.GreatestCommonDivisor;
 
 
 public class GCD {
 
     @Test
-    public void gcdTest() {
-        assertEquals(3, Utils.getGreatestCommonDivisor(15, 138));
-        assertEquals(79, Utils.getGreatestCommonDivisor(79, -79));
-        assertEquals(750, Utils.getGreatestCommonDivisor(-750*1000000009L, -750*123));
+    public void testRecursiveGCD() {
+        assertEquals(3, GreatestCommonDivisor.gcdUsingRecursion(15, 138));
+        assertEquals(79, GreatestCommonDivisor.gcdUsingRecursion(79, -79));
+        assertEquals(750, GreatestCommonDivisor.gcdUsingRecursion(-750*1000000009L, -750*123));
     }
+
+    @Test
+    public void testEuclideanGCD() {
+        long x = 1989;
+        long y = 867;
+        long gcd = GreatestCommonDivisor.gcdUsingEuclides(x, y);
+        long check = 51;
+        assertTrue("Euclids GCD error. expected="+check+" got="+gcd, (gcd==check));
+
+        x = 567;
+        y = 56;
+        gcd = GreatestCommonDivisor.gcdUsingEuclides(x, y);
+        check = 7;
+        assertTrue("Euclids GCD error. expected="+check+" got="+gcd, (gcd==check));
+
+        x = 10002345;
+        y = 67885;
+        gcd = GreatestCommonDivisor.gcdUsingEuclides(x, y);
+        check = 5;
+        assertTrue("Euclids GCD error. expected="+check+" got="+gcd, (gcd==check));
+    }
+
 }
