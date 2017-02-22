e81087399bf85d49e931db699d7e9638d8fca8f5
Justin Wetherell <phishman3579@gmail.com>
2017-02-22 17:56:25 -0500

    Cleaned up permutations pull request and combined with existing function

--- test/com/jwetherell/algorithms/mathematics/test/PermutationsTest.java
+++ test/com/jwetherell/algorithms/mathematics/test/PermutationsTest.java
@@ -1,11 +1,13 @@
 package com.jwetherell.algorithms.mathematics.test;
 
-import com.jwetherell.algorithms.mathematics.Permutations;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.util.LinkedList;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import com.jwetherell.algorithms.mathematics.Permutations;
 
 public class PermutationsTest {
 
@@ -13,47 +15,59 @@ public class PermutationsTest {
 	public void test1NumberOfPermutations() {
 		int numbers[] = {1,2,3,4};
 		int expectedNumberOfPermutations = 24;
-		LinkedList<LinkedList<Integer>> result = new LinkedList<LinkedList<Integer>>();
-		
-		assertEquals(expectedNumberOfPermutations, (Permutations.getAllPermutations(numbers,result)).size());
+		assertEquals(expectedNumberOfPermutations, (Permutations.getAllPermutations(numbers)).size());
 	}
 	
 	@Test
 	public void test2NumberOfPermutations() {
 		int numbers[] = {3,4,2};
 		int expectedNumberOfPermutations = 6;
-		LinkedList<LinkedList<Integer>> result = new LinkedList<LinkedList<Integer>>();
-		
-		assertEquals(expectedNumberOfPermutations, (Permutations.getAllPermutations(numbers,result)).size());
+		assertEquals(expectedNumberOfPermutations, (Permutations.getAllPermutations(numbers)).size());
 	}
 	
 	@Test
 	public void test3NumberOfPermutations() {
 		int numbers[] = {3,4,2,5,4,9};
 		int expectedNumberOfPermutations = 720;
-		LinkedList<LinkedList<Integer>> result = new LinkedList<LinkedList<Integer>>();
-		
-		assertEquals(expectedNumberOfPermutations, (Permutations.getAllPermutations(numbers,result)).size());
+		assertEquals(expectedNumberOfPermutations, (Permutations.getAllPermutations(numbers)).size());
 	}
 	
 	@Test
 	public void testComparePermutations() {
 		int numbers[] = {4,2};
-		
-		LinkedList<Integer> firstPermutation = new LinkedList<>();
+
+		LinkedList<Integer> firstPermutation = new LinkedList<Integer>();
 		firstPermutation.add(4);
 		firstPermutation.add(2);
-		
-		LinkedList<Integer> secondPermutation = new LinkedList<>();
+
+		LinkedList<Integer> secondPermutation = new LinkedList<Integer>();
 		secondPermutation.add(2);
 		secondPermutation.add(4);
-		
+
 		LinkedList<LinkedList<Integer>> allPermutations = new LinkedList<LinkedList<Integer>>();
 		allPermutations.add(firstPermutation);
 		allPermutations.add(secondPermutation);
-		
-		LinkedList<LinkedList<Integer>> result = new LinkedList<LinkedList<Integer>>();
-		
-		assertEquals(allPermutations, Permutations.getAllPermutations(numbers,result));
+
+		assertEquals(allPermutations, Permutations.getAllPermutations(numbers));
 	}
+
+    @Test
+    public void testPermutation1() {
+        final String string = "abc";
+        final String[] list = Permutations.permutations(string);
+        Assert.assertTrue(list[0].equals("abc"));
+        Assert.assertTrue(list[5].equals("cba"));
+    }
+
+    @Test
+    public void testPermutation2() {
+        final String string = "abcd";
+        final String[] list = Permutations.permutations(string);
+        Assert.assertTrue(list[0].equals("abcd"));
+        Assert.assertTrue(list[5].equals("adcb"));
+        Assert.assertTrue(list[11].equals("bdca"));
+        Assert.assertTrue(list[17].equals("cdba"));
+        Assert.assertTrue(list[23].equals("dcba"));
+    }
+
 }
