b27e2b158c5bf1ad5515d74d134986d69a053fb8
Justin Wetherell <phishman3579@gmail.com>
2017-08-09 15:19:59 -0400

    Added ternary search tree

---
+++ src/com/jwetherell/algorithms/data_structures/TernarySearchTree.java
@@ -0,0 +1,409 @@
+package com.jwetherell.algorithms.data_structures;
+
+import com.jwetherell.algorithms.data_structures.interfaces.ITree;
+
+/**
+ * In computer science, a ternary search tree is a type of trie (sometimes called a prefix tree) where nodes are arranged in a manner similar to a binary search tree, but with up to three 
+ * children rather than the binary tree's limit of two.
+ * <br>
+ * <a href="https://www.cs.upc.edu/~ps/downloads/tst/tst.html">This implementation is based on Jon Bentley and Bob Sedgewick's paper.</a>
+ * <p>
+ * @see <a href="https://en.wikipedia.org/wiki/Ternary_search_tree">Ternary Search Tree (Wikipedia)</a>
+ * <br>
+ * @author Justin Wetherell <phishman3579@gmail.com>
+ */
+public class TernarySearchTree<C extends CharSequence> implements ITree<C> {
+
+    protected INodeCreator creator;
+    protected Node root;
+
+    private int size = 0;
+
+    public TernarySearchTree() {
+        this.creator = new INodeCreator() {
+            /**
+             * {@inheritDoc}
+             */
+            @Override
+            public Node createNewNode(Node parent, Character character, boolean isWord) {
+                return (new Node(parent, character, isWord));
+            }
+        };
+    }
+
+    /**
+     * Constructor with external Node creator.
+     */
+    public TernarySearchTree(INodeCreator creator) {
+        this.creator = creator;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean add(C value) {
+        if (value == null)
+            return false;
+
+        final int before = size;
+        if (root == null) {
+            this.root = insert(null, null, value, 0);;
+        } else {
+            insert(null, root, value, 0);
+        }
+        final int after = size;
+        return (before<after);
+    }
+
+    private Node insert(Node parent, Node node, C value, int idx) {
+        if (idx >= value.length())
+            return null;
+
+        final char c = value.charAt(idx);
+        final boolean isWord = (idx==(value.length()-1));
+
+        if (node == null) {
+            // Create new node
+            node = this.creator.createNewNode(parent, c, isWord);
+
+            // If new node represents a "word", increase the size
+            if (isWord)
+                size++;
+        } else if (c==node.character && isWord && !node.isWord) {
+            // Changing an existing node into a "word" node
+            node.isWord = true;
+            // Increase the size
+            size++;
+        }
+
+        if (c < node.character) {
+            node.loKid = insert(node, node.loKid, value, idx);
+        } else if (c > node.character) {
+            node.hiKid = insert(node, node.hiKid, value, idx);
+        } else if (idx < (value.length()-1)) {
+            // Not done with whole string yet
+            node.kid = insert(node, node.kid, value, ++idx);
+        }
+        return node;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public C remove(C value) {
+        if (value == null || root == null)
+            return null;
+
+        // Find the node
+        final Node node = search(root, value, 0);
+
+        // If node was found, remove from tree
+        if (node != null) {
+            if (node.isWord) {
+                node.isWord = false;
+                remove(node);
+                size--;
+                return value;
+            }
+        }
+        return null;
+    }
+
+    private void remove(Node node) {
+        final Node parent = node.parent;
+        // If node does not represent a word and has no children
+        if (!node.isWord && node.loKid==null && node.kid==null && node.hiKid==null) {
+            // Remove node from parent
+            if (parent!=null && parent.loKid==node) {
+                parent.loKid = null;
+            } else if (parent!=null && parent.hiKid==node) {
+                parent.hiKid = null;
+            } else if (parent!=null && parent.kid==node) {
+                parent.kid = null;
+            }
+
+            if (parent != null) {
+                // Go up the tree and prune
+                remove(parent);
+            } else {
+                // If node doesn't have a parent, it's root.
+                this.root = null;
+            }
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void clear() {
+        root = null;
+        size = 0;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean contains(C value) {
+        if (value == null)
+            return false;
+
+        // Find the node
+        final Node node = search(root, value, 0);
+        // If node isn't null and it represents a "word" then the tree contains the value
+        return (node!=null && node.isWord);
+    }
+
+    private Node search(Node node, C value, int idx) {
+        if (node == null || idx>=value.length())
+            return null;
+
+        final char c = value.charAt(idx);
+
+        if (c < node.character)
+            return search(node.loKid, value, idx);
+        if (c > node.character)
+            return search(node.hiKid, value, idx);
+        if (++idx < value.length()) 
+            return search(node.kid, value, idx);
+        return node;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean validate() {
+        if (this.root == null)
+            return true;
+
+        return validate(root);
+    }
+
+    private boolean validate(Node node) {
+        if (node.loKid != null) {
+            if (node.loKid.character >= node.character)
+                return false;
+            return validate(node.loKid);
+        }
+        if (node.kid != null) {
+            return validate(node.kid);
+        }
+        if (node.hiKid != null) {
+            if (node.hiKid.character <= node.character)
+                return false;
+            return validate(node.hiKid);
+        }
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public java.util.Collection<C> toCollection() {
+        return (new JavaCompatibleTree<C>(this));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+        return TreePrinter.getString(this);
+    }
+
+    protected static interface INodeCreator {
+
+        /**
+         * Create a new node for sequence.
+         * 
+         * @param parent
+         *            node of the new node.
+         * @param character
+         *            which represents this node.
+         * @param isWord
+         *            signifies if the node represents a word.
+         * @return Node which was created.
+         */
+        public Node createNewNode(Node parent, Character character, boolean isWord);
+    }
+
+    protected static class TreePrinter {
+
+        public static <C extends CharSequence> void print(TernarySearchTree<C> tree) {
+            System.out.println(getString(tree));
+        }
+
+        public static <C extends CharSequence> String getString(TernarySearchTree<C> tree) {
+            if (tree.root == null) 
+                return "Tree has no nodes.";
+            return getString(tree.root, "", null, true);
+        }
+
+        protected static String getString(Node node, String prefix, String previousString, boolean isTail) {
+            final StringBuilder builder = new StringBuilder();
+            final String string;
+            final String temp = String.valueOf(node.character);
+            if (previousString != null)
+                string = previousString + temp;
+            else
+                string = temp;
+            builder.append(prefix + (isTail ? "└── " : "├── ") + ((node.isWord == true) ? 
+                              ("(" + node.character + ") " + string)
+                          : 
+                              node.character) + "\n"
+            );
+            if (node.loKid != null)
+                builder.append(getString(node.loKid, prefix + (isTail ? "    " : "│   "), string.substring(0, string.length()-1), false));
+            if (node.kid != null)
+                builder.append(getString(node.kid, prefix + (isTail ? "    " : "│   "), string, false));
+            if (node.hiKid != null)
+                builder.append(getString(node.hiKid, prefix + (isTail ? "    " : "│   "), string.substring(0, string.length()-1), true));
+            return builder.toString();
+        }
+    }
+
+    protected static class Node {
+
+        private final Node parent;
+        private final char character;
+
+        private boolean isWord;
+
+        protected Node loKid;
+        protected Node kid;
+        protected Node hiKid;
+
+        protected Node(Node parent, char character, boolean isWord) {
+            this.parent = parent;
+            this.character = character;
+            this.isWord = isWord;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static class JavaCompatibleTree<C extends CharSequence> extends java.util.AbstractCollection<C> {
+
+        private TernarySearchTree<C> tree = null;
+
+        public JavaCompatibleTree(TernarySearchTree<C> tree) {
+            this.tree = tree;
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public boolean add(C value) {
+            return tree.add(value);
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public boolean remove(Object value) {
+            return (tree.remove((C)value)!=null);
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public boolean contains(Object value) {
+            return tree.contains((C)value);
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public int size() {
+            return tree.size;
+        }
+
+        /**
+         * {@inheritDoc}
+         * 
+         * WARNING: This iterator makes a copy of the tree's contents during it's construction!
+         */
+        @Override
+        public java.util.Iterator<C> iterator() {
+            return (new TreeIterator<C>(tree));
+        }
+
+        private static class TreeIterator<C extends CharSequence> implements java.util.Iterator<C> {
+
+            private TernarySearchTree<C> tree = null;
+            private TernarySearchTree.Node lastNode = null;
+            private java.util.Iterator<java.util.Map.Entry<Node, String>> iterator = null;
+
+            protected TreeIterator(TernarySearchTree<C> tree) {
+                this.tree = tree;
+                java.util.Map<TernarySearchTree.Node,String> map = new java.util.LinkedHashMap<TernarySearchTree.Node,String>();
+                if (this.tree.root!=null) {
+                    getNodesWhichRepresentsWords(this.tree.root,"",map);
+                }
+                iterator = map.entrySet().iterator();
+            }
+
+            private void getNodesWhichRepresentsWords(TernarySearchTree.Node node, String string, java.util.Map<TernarySearchTree.Node,String> nodesMap) {
+                StringBuilder builder = new StringBuilder(string);
+                builder.append(node.character);
+                if (node.isWord) 
+                    nodesMap.put(node,builder.toString());
+                if (node.loKid != null) {
+                    Node child = node.loKid;
+                    getNodesWhichRepresentsWords(child, builder.subSequence(0, builder.length()-1).toString(), nodesMap);
+                }
+                if (node.kid != null) {
+                    Node child = node.kid;
+                    getNodesWhichRepresentsWords(child, builder.toString(), nodesMap);
+                }
+                if (node.hiKid != null) {
+                    Node child = node.hiKid;
+                    getNodesWhichRepresentsWords(child, builder.subSequence(0, builder.length()-1).toString(), nodesMap);
+                }
+            }
+
+            /**
+             * {@inheritDoc}
+             */
+            @Override
+            public boolean hasNext() {
+                if (iterator!=null && iterator.hasNext()) 
+                    return true;
+                return false;
+            }
+
+            /**
+             * {@inheritDoc}
+             */
+            @Override
+            public C next() {
+                if (iterator==null) return null;
+
+                java.util.Map.Entry<TernarySearchTree.Node,String> entry = iterator.next();
+                lastNode = entry.getKey();
+                return (C)entry.getValue();
+            }
+
+            /**
+             * {@inheritDoc}
+             */
+            @Override
+            public void remove() {
+                if (iterator==null || tree==null) return;
+
+                iterator.remove();
+                this.tree.remove(lastNode);
+            }
+        }
+    }
+}
---
+++ test/com/jwetherell/algorithms/data_structures/test/TernarySearchTreeTests.java
@@ -0,0 +1,75 @@
+package com.jwetherell.algorithms.data_structures.test;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import com.jwetherell.algorithms.data_structures.TernarySearchTree;
+import com.jwetherell.algorithms.data_structures.test.common.JavaCollectionTest;
+import com.jwetherell.algorithms.data_structures.test.common.TreeTest;
+import com.jwetherell.algorithms.data_structures.test.common.Utils;
+import com.jwetherell.algorithms.data_structures.test.common.Utils.TestData;
+
+public class TernarySearchTreeTests {
+
+    @Test
+    public void test1() {
+        final List<String> tests = new ArrayList<String>();
+        tests.add("BEE");
+        tests.add("BEEN");
+        tests.add("BEAST");
+        tests.add("BELOW");
+        tests.add("BEFORE");
+        tests.add("BUT");
+        tests.add("CAT");
+        tests.add("BE");
+        tests.add("B");
+
+        final TernarySearchTree<String> bst = new TernarySearchTree<String>();
+
+        // Add
+        bst.add(null);
+        bst.add("");
+        for (String s : tests)
+            bst.add(s);
+        bst.add(null);
+        bst.add("");
+        Assert.assertFalse(bst.add("BE"));
+
+        // contains
+        Assert.assertFalse(bst.contains(null));
+        Assert.assertFalse(bst.contains(""));
+        for (String s : tests)
+            Assert.assertTrue(bst.contains(s));
+        Assert.assertFalse(bst.contains(null));
+        Assert.assertFalse(bst.contains(""));
+
+        // remove
+        Assert.assertTrue(bst.remove(null)==null);
+        Assert.assertTrue(bst.remove("")==null);
+        for (String s : tests)
+            Assert.assertTrue(bst.remove(s)!=null);
+        Assert.assertTrue(bst.remove(null)==null);
+        Assert.assertTrue(bst.remove("")==null);
+    }
+
+    @Test
+    public void testTernary() {
+        TestData data = Utils.generateTestData(1000);
+
+        String bstName = "TernarySearchTreeTests";
+        TernarySearchTree<String> bst = new TernarySearchTree<String>();
+        Collection<String> bstCollection = bst.toCollection();
+
+        assertTrue(TreeTest.testTree(bst, String.class, bstName,
+                                     data.unsorted, data.invalid));
+        assertTrue(JavaCollectionTest.testCollection(bstCollection, String.class, bstName,
+                                                     data.unsorted, data.sorted, data.invalid));
+    }
+}
