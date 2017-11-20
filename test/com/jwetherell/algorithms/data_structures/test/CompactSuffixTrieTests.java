616213e71c4b8416764a6f32cda9396c85c2f5b0
ParkChangHan <qkck@naver.com>
2017-11-20 16:17:53 +0900

    Test unused Method

--- test/com/jwetherell/algorithms/data_structures/test/CompactSuffixTrieTests.java
+++ test/com/jwetherell/algorithms/data_structures/test/CompactSuffixTrieTests.java
@@ -25,4 +25,26 @@ public class CompactSuffixTrieTests {
         exists = trie.doesSubStringExist(pass);
         assertTrue("YIKES!! " + pass + " doesn't exists.", exists);
     }
+
+    @Test
+    public void testCompactSuffixTrie_equals() {
+        String bookkeeper = "bookkeeper";
+        CompactSuffixTrie<String> trie = new CompactSuffixTrie<String>(bookkeeper);
+
+        String bookkeeper_1 = "bookkeeper";
+        CompactSuffixTrie<String> trie_1 = new CompactSuffixTrie<String>(bookkeeper_1);
+
+        boolean equal = trie.equals(trie_1);
+        assertTrue("YIKES!! " + bookkeeper + " and " + bookkeeper_1 + " are not equal.", equal);
+
+
+        String failed = "failed";
+        trie = new CompactSuffixTrie<String>(failed);
+
+        String failed_1 = "failet";
+        trie_1 = new CompactSuffixTrie<String>(failed_1);
+
+        equal = trie.equals(trie_1);
+        assertFalse("YIKES!! " + failed + " and " + failed_1 + " are equal.", equal);
+    }
 }
