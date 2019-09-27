c6e74eb5a328a5fd5eb099d8229fc5971ea5abc1
kaori9x <31662398+kaori9x@users.noreply.github.com>
2019-09-27 15:25:47 +0700

    Add files via upload

---
+++ test/com/jwetherell/algorithms/mathematics/test/MultiplicationTest2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kiem_thu;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Dung
+ */
+public class MultiplicationTest {
+    @Test
+    public void testMultiplyUsingShift1() {
+        assertEquals(90, Multiplication.multiplyUsingShift(10, 9));
+    }
+    @Test
+    public void testMultiplyUsingShift2() {
+        assertEquals(-90, Multiplication.multiplyUsingShift(10, -9));
+    }
+
+
+}
