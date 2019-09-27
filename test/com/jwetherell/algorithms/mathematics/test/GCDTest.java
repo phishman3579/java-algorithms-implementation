d2b316219d4dd3ce12c4b66584699c4f9e64467c
doanthihoaithu <hoaithuxt1998@gmail.com>
2019-09-27 16:45:42 +0700

    refactor testcases for GCD Algorithms

--- test/com/jwetherell/algorithms/mathematics/test/GCDTest.java
+++ test/com/jwetherell/algorithms/mathematics/test/GCDTest.java
@@ -39,97 +39,128 @@ public class GCDTest {
     }
 
     @Test
-    public void testRecursiveGCDWithSmallestBoundaryValue() {
+    public void testRecursiveGCDWithSmallestBoundaryValue_1() {
         long x = Long.MIN_VALUE;
         long y = 4611686018427387900L;
         long gcd = GreatestCommonDivisor.gcdUsingRecursion(x, y);
         long check = 4;
         assertTrue("Euclids GCD error. expected=" + check + " got=" + gcd, (gcd == check));
-
-        x = Long.MIN_VALUE + 1;
-        y = 4611686018427387900L;
-        gcd = GreatestCommonDivisor.gcdUsingRecursion(x, y);
-        check = 7;
-        assertTrue("Euclids GCD error. expected=" + check + " got=" + gcd, (gcd == check));
-
-        x = 60060L;
-        y = Long.MIN_VALUE;
-        gcd = GreatestCommonDivisor.gcdUsingRecursion(x, y);
-        check = 4;
-        assertTrue("Euclids GCD error. expected=" + check + " got=" + gcd, (gcd == check));
-
-        x = 60060L;
-        y = Long.MIN_VALUE + 1;
-        gcd = GreatestCommonDivisor.gcdUsingRecursion(x, y);
-        check = 7;
-        assertTrue("Euclids GCD error. expected=" + check + " got=" + gcd, (gcd == check));
-
     }
 
     @Test
-    public void testRecursiveGCDWithBiggestBoundaryValue() {
+    public void testRecursiveGCDWithSmallestBoundaryValue_2() {
+        long x = Long.MIN_VALUE + 1;
+        long y = 4611686018427387900L;
+        long gcd = GreatestCommonDivisor.gcdUsingRecursion(x, y);
+        long check = 7;
+        assertTrue("Euclids GCD error. expected=" + check + " got=" + gcd, (gcd == check));
+    }
+
+    @Test
+    public void testRecursiveGCDWithSmallestBoundaryValue_3() {
+        long x = 60060L;
+        long y = Long.MIN_VALUE;
+        long gcd = GreatestCommonDivisor.gcdUsingRecursion(x, y);
+        long check = 4;
+        assertTrue("Euclids GCD error. expected=" + check + " got=" + gcd, (gcd == check));
+    }
+
+    @Test
+    public void testRecursiveGCDWithSmallestBoundaryValue_4() {
+        long x = 60060L;
+        long y = Long.MIN_VALUE + 1;
+        long gcd = GreatestCommonDivisor.gcdUsingRecursion(x, y);
+        long check = 7;
+        assertTrue("Euclids GCD error. expected=" + check + " got=" + gcd, (gcd == check));
+    }
+
+    @Test
+    public void testRecursiveGCDWithBiggestBoundaryValue_1() {
         long x = Long.MAX_VALUE;
         long y = 4611686018427387900L;
         long gcd = GreatestCommonDivisor.gcdUsingRecursion(x, y);
         long check = 7;
         assertTrue("Euclids GCD error. expected=" + check + " got=" + gcd, (gcd == check));
-
-        x = Long.MAX_VALUE - 1;
-        y = 4611686018427387900L;
-        gcd = GreatestCommonDivisor.gcdUsingRecursion(x, y);
-        check = 6;
-        assertTrue("Euclids GCD error. expected=" + check + " got=" + gcd, (gcd == check));
-
-        x = 124200L;
-        y = Long.MAX_VALUE;
-        gcd = GreatestCommonDivisor.gcdUsingRecursion(x, y);
-        check = 1;
-        assertTrue("Euclids GCD error. expected=" + check + " got=" + gcd, (gcd == check));
-
-        x = 124200L;
-        y = Long.MAX_VALUE - 1;
-        gcd = GreatestCommonDivisor.gcdUsingRecursion(x, y);
-        check = 6;
-        assertTrue("Euclids GCD error. expected=" + check + " got=" + gcd, (gcd == check));
-
     }
 
     @Test
-    public void testRecursiveGCDWithSpecialValue() {
+    public void testRecursiveGCDWithBiggestBoundaryValue_2() {
+        long x = Long.MAX_VALUE - 1;
+        long y = 4611686018427387900L;
+        long gcd = GreatestCommonDivisor.gcdUsingRecursion(x, y);
+        long check = 6;
+        assertTrue("Euclids GCD error. expected=" + check + " got=" + gcd, (gcd == check));
+    }
+
+    @Test
+    public void testRecursiveGCDWithBiggestBoundaryValue_3() {
+        long x = 124200L;
+        long y = Long.MAX_VALUE;
+        long gcd = GreatestCommonDivisor.gcdUsingRecursion(x, y);
+        long check = 1;
+        assertTrue("Euclids GCD error. expected=" + check + " got=" + gcd, (gcd == check));
+    }
+
+    @Test
+    public void testRecursiveGCDWithBiggestBoundaryValue_4() {
+        long x = 124200L;
+        long y = Long.MAX_VALUE - 1;
+        long gcd = GreatestCommonDivisor.gcdUsingRecursion(x, y);
+        long check = 6;
+        assertTrue("Euclids GCD error. expected=" + check + " got=" + gcd, (gcd == check));
+    }
+
+    @Test
+    public void testRecursiveGCDWithSpecialValue_1() {
         long x = Long.MAX_VALUE;
         long y = Long.MAX_VALUE -1;
         long gcd = GreatestCommonDivisor.gcdUsingRecursion(x, y);
         long check = 1;
         assertTrue("Euclids GCD error. expected=" + check + " got=" + gcd, (gcd == check));
+    }
 
-        x = Long.MIN_VALUE;
-        y = Long.MIN_VALUE +1;
-        gcd = GreatestCommonDivisor.gcdUsingRecursion(x, y);
-        check = 1;
+    @Test
+    public void testRecursiveGCDWithSpecialValue_2() {
+        long x = Long.MIN_VALUE;
+        long y = Long.MIN_VALUE +1;
+        long gcd = GreatestCommonDivisor.gcdUsingRecursion(x, y);
+        long check = 1;
         assertTrue("Euclids GCD error. expected=" + check + " got=" + gcd, (gcd == check));
+    }
 
-        x = 0;
-        y = Long.MIN_VALUE;
-        gcd = GreatestCommonDivisor.gcdUsingRecursion(x, y);
-        check = -9223372036854775808L;
+    @Test
+    public void testRecursiveGCDWithSpecialValue_3() {
+        long x = 0;
+        long y = Long.MIN_VALUE;
+        long gcd = GreatestCommonDivisor.gcdUsingRecursion(x, y);
+        long check = -9223372036854775808L;
         assertTrue("Euclids GCD error. expected=" + check + " got=" + gcd, (gcd == check));
+    }
 
-        x = 1;
-        y = Long.MIN_VALUE;
-        gcd = GreatestCommonDivisor.gcdUsingRecursion(x, y);
-        check = 1;
+    @Test
+    public void testRecursiveGCDWithSpecialValue_4() {
+        long x = 1;
+        long y = Long.MIN_VALUE;
+        long gcd = GreatestCommonDivisor.gcdUsingRecursion(x, y);
+        long check = 1;
         assertTrue("Euclids GCD error. expected=" + check + " got=" + gcd, (gcd == check));
+    }
 
-        x = 0;
-        y = Long.MAX_VALUE;
-        gcd = GreatestCommonDivisor.gcdUsingRecursion(x, y);
-        check = 9223372036854775807L;
+    @Test
+    public void testRecursiveGCDWithSpecialValue_5() {
+        long x = 0;
+        long y = Long.MAX_VALUE;
+        long gcd = GreatestCommonDivisor.gcdUsingRecursion(x, y);
+        long check = 9223372036854775807L;
         assertTrue("Euclids GCD error. expected=" + check + " got=" + gcd, (gcd == check));
+    }
 
-        x = 1;
-        y = Long.MAX_VALUE;
-        gcd = GreatestCommonDivisor.gcdUsingRecursion(x, y);
-        check = 1;
+    @Test
+    public void testRecursiveGCDWithSpecialValue_6() {
+        long x = 6464500;
+        long y = 1546340;
+        long gcd = GreatestCommonDivisor.gcdUsingRecursion(x, y);
+        long check = 1;
         assertTrue("Euclids GCD error. expected=" + check + " got=" + gcd, (gcd == check));
     }
 }
