34830ab559c9d02286d95471d0057477def9ee5d
Pragyan Varshney <37217831+varshneypragyan@users.noreply.github.com>
2019-10-17 00:09:52 +0530

    Add files via upload

--- src/com/jwetherell/algorithms/sorts/CountingSort.java
+++ src/com/jwetherell/algorithms/sorts/CountingSort.java
@@ -1,60 +1,54 @@
-package com.jwetherell.algorithms.sorts;
+// Java implementation of Counting Sort 
+class CountingSort 
+{ 
+	void sort(char arr[]) 
+	{ 
+		int n = arr.length; 
 
-/**
- * Counting sort is an algorithm for sorting a collection of objects according
- * to keys that are small integers; that is, it is an integer sorting algorithm.
- * It operates by counting the number of objects that have each distinct key
- * value, and using arithmetic on those counts to determine the positions of
- * each key value in the output sequence. 
- * <p>
- * Family: Counting.<br>
- * Space: An Array of length r.<br> 
- * Stable: True.<br>
- * <p>
- * Average case = O(n+r)<br>
- * Worst case = O(n+r)<br>
- * Best case = O(n+r)<br>
- * <p> 
- * NOTE: r is the range of numbers (0 to r) to be sorted.
- * <p>
- * @see <a href="https://en.wikipedia.org/wiki/Counting_sort">Counting Sort (Wikipedia)</a>
- * <br>
- * @author Justin Wetherell <phishman3579@gmail.com>
- */
-public class CountingSort {
+		// The output character array that will have sorted arr 
+		char output[] = new char[n]; 
 
-    private CountingSort() { }
+		// Create a count array to store count of inidividul 
+		// characters and initialize count array as 0 
+		int count[] = new int[256]; 
+		for (int i=0; i<256; ++i) 
+			count[i] = 0; 
 
-    public static Integer[] sort(Integer[] unsorted) {
-        int maxValue = findMax(unsorted);
-        int[] counts = new int[maxValue + 1];
-        updateCounts(unsorted, counts);
-        populateCounts(unsorted, counts);
-        return unsorted;
-    }
+		// store count of each character 
+		for (int i=0; i<n; ++i) 
+			++count[arr[i]]; 
 
-    private static int findMax(Integer[] unsorted) {
-        int max = Integer.MIN_VALUE;
-        for (int i : unsorted) {
-            if (i > max)
-                max = i;
-        }
-        return max;
-    }
+		// Change count[i] so that count[i] now contains actual 
+		// position of this character in output array 
+		for (int i=1; i<=255; ++i) 
+			count[i] += count[i-1]; 
 
-    private static void updateCounts(Integer[] unsorted, int[] counts) {
-        for (int e : unsorted)
-            counts[e]++;
-    }
+		// Build the output character array 
+		// To make it stable we are operating in reverse order. 
+		for (int i = n-1; i>=0; i--) 
+		{ 
+			output[count[arr[i]]-1] = arr[i]; 
+			--count[arr[i]]; 
+		} 
 
-    private static void populateCounts(Integer[] unsorted, int[] counts) {
-        int index = 0;
-        for (int i = 0; i < counts.length; i++) {
-            int e = counts[i];
-            while (e > 0) {
-                unsorted[index++] = i;
-                e--;
-            }
-        }
-    }
+		// Copy the output array to arr, so that arr now 
+		// contains sorted characters 
+		for (int i = 0; i<n; ++i) 
+			arr[i] = output[i]; 
+	} 
+
+	// Driver method 
+	public static void main(String args[]) 
+	{ 
+		CountingSort ob = new CountingSort(); 
+		char arr[] = {'c', 'o', 'u', 'n', 't', 'i', 'n', 
+					'g', 's', 'o', 'r', 't', 's'
+					}; 
+
+		ob.sort(arr); 
+
+		System.out.print("Sorted character array is "); 
+		for (int i=0; i<arr.length; ++i) 
+			System.out.print(arr[i]); 
+	} 
 }
