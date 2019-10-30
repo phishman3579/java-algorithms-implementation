38d06ee316907262b20ba695f99c129e14eadef4
gauravdarbhanga <57192490+gauravdarbhanga@users.noreply.github.com>
2019-10-30 23:59:12 +0530

    Update CountingSort.java
    Did comment on the method.

--- src/com/jwetherell/algorithms/sorts/CountingSort.java
+++ src/com/jwetherell/algorithms/sorts/CountingSort.java
@@ -27,26 +27,26 @@ public class CountingSort {
 
     public static Integer[] sort(Integer[] unsorted) {
         int maxValue = findMax(unsorted);
-        int[] counts = new int[maxValue + 1];
+        int[] counts = new int[maxValue + 1];//counts number of elements
         updateCounts(unsorted, counts);
         populateCounts(unsorted, counts);
         return unsorted;
     }
-
+    //finding maximum value in unsorted array 
     private static int findMax(Integer[] unsorted) {
-        int max = Integer.MIN_VALUE;
+        int max = Integer.MIN_VALUE;//assume minimum value(-2147483648) of interger is maximum
         for (int i : unsorted) {
             if (i > max)
                 max = i;
         }
         return max;
     }
-
+    //Incrementing the number of counts in unsorted array 
     private static void updateCounts(Integer[] unsorted, int[] counts) {
         for (int e : unsorted)
             counts[e]++;
     }
-
+    
     private static void populateCounts(Integer[] unsorted, int[] counts) {
         int index = 0;
         for (int i = 0; i < counts.length; i++) {
