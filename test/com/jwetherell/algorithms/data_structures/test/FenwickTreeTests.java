b656c9424ed7329b937935fa6e3c2a1f90d5a71a
Beatrice Berg <72010841+reversedpeach@users.noreply.github.com>
2022-03-02 20:18:14 +0100

    Fixed #97. test: Added branch coverage tests for all TODO-functions in FenwickTree.java (#102)

--- test/com/jwetherell/algorithms/data_structures/test/FenwickTreeTests.java
+++ test/com/jwetherell/algorithms/data_structures/test/FenwickTreeTests.java
@@ -1,5 +1,9 @@
 package com.jwetherell.algorithms.data_structures.test;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Comparator;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -42,6 +46,7 @@ public class FenwickTreeTests {
         Collections.sort(segments,REVERSE);
         testRangeSumFenwickTree(segments);
     }
+
 
     private void testRangeSumFenwickTree(List<FenwickTree.Data.RangeSumData<Integer>> segments) {   // Range Sum Segment tree
         FenwickTree<FenwickTree.Data.RangeSumData<Integer>> tree = new FenwickTree<FenwickTree.Data.RangeSumData<Integer>>(segments);
@@ -136,4 +141,272 @@ public class FenwickTreeTests {
             toString = "\n"+obj.toString();
         Assert.assertTrue(msg+toString, isTrue);
     }
+
+    // Requirement: 
+    // When FenwickTree consists of data with BigDecimal numbers it should be able to sum them up correctly
+   @Test
+    public void testBigDecimalFenwickTree() {
+        List<FenwickTree.Data.RangeSumData<BigDecimal>> segments = new ArrayList<FenwickTree.Data.RangeSumData<BigDecimal>>();
+        segments.add(new FenwickTree.Data.RangeSumData<BigDecimal>(0,  (BigDecimal) new BigDecimal(1.1)));
+        segments.add(new FenwickTree.Data.RangeSumData<BigDecimal>(1,  (BigDecimal) new BigDecimal(2.1)));
+        segments.add(new FenwickTree.Data.RangeSumData<BigDecimal>(2,  (BigDecimal) new BigDecimal(6.1)));
+        segments.add(new FenwickTree.Data.RangeSumData<BigDecimal>(3,  (BigDecimal) new BigDecimal(7.1)));
+
+        // No matter which order the data is given, all tests should pass
+
+        // Initial order.
+        testBigDecimalFenwickTree(segments);
+
+        // Randomize it
+        Collections.shuffle(segments);
+        testBigDecimalFenwickTree(segments);
+
+        // Try in order
+        Collections.sort(segments);
+        testBigDecimalFenwickTree(segments);
+
+        // Try reverse order
+        Collections.sort(segments,REVERSE);
+        testBigDecimalFenwickTree(segments);
+    }
+
+     private void testBigDecimalFenwickTree(List<FenwickTree.Data.RangeSumData<BigDecimal>> segments) {   // Range Sum Segment tree
+        FenwickTree<FenwickTree.Data.RangeSumData<BigDecimal>> tree = new FenwickTree<FenwickTree.Data.RangeSumData<BigDecimal>>(segments);
+
+        FenwickTree.Data.RangeSumData<BigDecimal> query = tree.query(0, 3);
+ 
+        query = tree.query(2);
+        assertTrue("Segment tree query error. query=2 result="+query, tree, query.sum.compareTo(new BigDecimal(6.1)) == 0);
+
+        query = tree.query(0, 1); // stabbing
+        assertTrue("Segment tree query error. query=0->1 result="+ query, tree, query.sum.compareTo(new BigDecimal(1.1).add(new BigDecimal(2.1))) == 0);
+    }
+
+    // Requirement: 
+    // When FenwickTree consists of data with BigInteger numbers it should be able to sum them up correctly
+    @Test
+    public void testBigIntegerFenwickTree() {
+        List<FenwickTree.Data.RangeSumData<BigInteger>> segments = new ArrayList<FenwickTree.Data.RangeSumData<BigInteger>>();
+        segments.add(new FenwickTree.Data.RangeSumData<BigInteger>(0,  (BigInteger) BigInteger.valueOf(1)));
+        segments.add(new FenwickTree.Data.RangeSumData<BigInteger>(1,  (BigInteger) BigInteger.valueOf(2)));
+        segments.add(new FenwickTree.Data.RangeSumData<BigInteger>(2,  (BigInteger) BigInteger.valueOf(6)));
+        segments.add(new FenwickTree.Data.RangeSumData<BigInteger>(3,  (BigInteger) BigInteger.valueOf(7)));
+
+        // No matter which order the data is given, all tests should pass
+
+        // Initial order.
+        testBigIntegerFenwickTree(segments);
+
+        // Randomize it
+        Collections.shuffle(segments);
+        testBigIntegerFenwickTree(segments);
+
+        // Try in order
+        Collections.sort(segments);
+        testBigIntegerFenwickTree(segments);
+
+        // Try reverse order
+        Collections.sort(segments,REVERSE);
+        testBigIntegerFenwickTree(segments);
+    }
+
+     private void testBigIntegerFenwickTree(List<FenwickTree.Data.RangeSumData<BigInteger>> segments) {   // Range Sum Segment tree
+        FenwickTree<FenwickTree.Data.RangeSumData<BigInteger>> tree = new FenwickTree<FenwickTree.Data.RangeSumData<BigInteger>>(segments);
+
+        FenwickTree.Data.RangeSumData<BigInteger> query = tree.query(0, 3);
+ 
+        query = tree.query(2);
+        assertTrue("Segment tree query error. query=2 result="+query, tree, query.sum.compareTo(BigInteger.valueOf(6)) == 0);
+
+        query = tree.query(0, 1); // stabbing
+        assertTrue("Segment tree query error. query=0->1 result="+ query, tree, query.sum.compareTo(BigInteger.valueOf(1).add(BigInteger.valueOf(2))) == 0);
+    }
+
+    // Requirement: 
+    // When FenwickTree consists of data with Long numbers it should be able to sum them up correctly
+    @Test
+    public void testLongFenwickTree() {
+        List<FenwickTree.Data.RangeSumData<Long>> segments = new ArrayList<FenwickTree.Data.RangeSumData<Long>>();
+        segments.add(new FenwickTree.Data.RangeSumData<Long>(0,  (Long) Long.valueOf(1)));
+        segments.add(new FenwickTree.Data.RangeSumData<Long>(1,  (Long) Long.valueOf(2)));
+        segments.add(new FenwickTree.Data.RangeSumData<Long>(2,  (Long) Long.valueOf(6)));
+        segments.add(new FenwickTree.Data.RangeSumData<Long>(3,  (Long) Long.valueOf(7)));
+
+        // No matter which order the data is given, all tests should pass
+
+        // Initial order.
+        testLongFenwickTree(segments);
+
+        // Randomize it
+        Collections.shuffle(segments);
+        testLongFenwickTree(segments);
+
+        // Try in order
+        Collections.sort(segments);
+        testLongFenwickTree(segments);
+
+        // Try reverse order
+        Collections.sort(segments,REVERSE);
+        testLongFenwickTree(segments);
+    }
+
+     private void testLongFenwickTree(List<FenwickTree.Data.RangeSumData<Long>> segments) {   // Range Sum Segment tree
+        FenwickTree<FenwickTree.Data.RangeSumData<Long>> tree = new FenwickTree<FenwickTree.Data.RangeSumData<Long>>(segments);
+
+        FenwickTree.Data.RangeSumData<Long> query = tree.query(0, 3);
+ 
+        query = tree.query(2);
+        assertTrue("Segment tree query error. query=2 result="+query, tree, query.sum.compareTo(Long.valueOf(6)) == 0);
+
+        query = tree.query(0, 1); // stabbing
+        assertTrue("Segment tree query error. query=0->1 result="+ query, tree, query.sum.compareTo(Long.sum(Long.valueOf(1), (Long.valueOf(2)))) == 0);
+    }
+
+    // Requirement: 
+    // When FenwickTree consists of data with Double numbers it should be able to sum them up correctly
+    @Test
+    public void testDoubleFenwickTree() {
+        List<FenwickTree.Data.RangeSumData<Double>> segments = new ArrayList<FenwickTree.Data.RangeSumData<Double>>();
+        segments.add(new FenwickTree.Data.RangeSumData<Double>(0,  (Double) new Double(1.11)));
+        segments.add(new FenwickTree.Data.RangeSumData<Double>(1,  (Double) new Double(2.11)));
+        segments.add(new FenwickTree.Data.RangeSumData<Double>(2,  (Double) new Double(6.11)));
+        segments.add(new FenwickTree.Data.RangeSumData<Double>(3,  (Double) new Double(7.11)));
+
+        // No matter which order the data is given, all tests should pass
+
+        // Initial order.
+        testDoubleFenwickTree(segments);
+
+        // Randomize it
+        Collections.shuffle(segments);
+        testDoubleFenwickTree(segments);
+
+        // Try in order
+        Collections.sort(segments);
+        testDoubleFenwickTree(segments);
+
+        // Try reverse order
+        Collections.sort(segments,REVERSE);
+        testDoubleFenwickTree(segments);
+    }
+
+     private void testDoubleFenwickTree(List<FenwickTree.Data.RangeSumData<Double>> segments) {   // Range Sum Segment tree
+        FenwickTree<FenwickTree.Data.RangeSumData<Double>> tree = new FenwickTree<FenwickTree.Data.RangeSumData<Double>>(segments);
+
+        FenwickTree.Data.RangeSumData<Double> query = tree.query(0, 3);
+ 
+        query = tree.query(2);
+        assertTrue("Segment tree query error. query=2 result="+query, tree, query.sum.compareTo(new Double(6.11)) == 0);
+
+        query = tree.query(0, 1); // stabbing
+        assertTrue("Segment tree query error. query=0->1 result="+ query, tree, query.sum.compareTo(Double.sum(new Double(1.11),(new Double(2.11)))) == 0);
+    }
+
+    // Requirement: 
+    // When FenwickTree consists of data with Float numbers it should be able to sum them up correctly
+    @Test
+    public void testFloatFenwickTree() {
+        List<FenwickTree.Data.RangeSumData<Float>> segments = new ArrayList<FenwickTree.Data.RangeSumData<Float>>();
+        segments.add(new FenwickTree.Data.RangeSumData<Float>(0,  (Float) new Float(1.11)));
+        segments.add(new FenwickTree.Data.RangeSumData<Float>(1,  (Float) new Float(2.11)));
+        segments.add(new FenwickTree.Data.RangeSumData<Float>(2,  (Float) new Float(6.11)));
+        segments.add(new FenwickTree.Data.RangeSumData<Float>(3,  (Float) new Float(7.11)));
+
+        // No matter which order the data is given, all tests should pass
+
+        // Initial order.
+        testFloatFenwickTree(segments);
+
+        // Randomize it
+        Collections.shuffle(segments);
+        testFloatFenwickTree(segments);
+
+        // Try in order
+        Collections.sort(segments);
+        testFloatFenwickTree(segments);
+
+        // Try reverse order
+        Collections.sort(segments,REVERSE);
+        testFloatFenwickTree(segments);
+    }
+
+     private void testFloatFenwickTree(List<FenwickTree.Data.RangeSumData<Float>> segments) {   // Range Sum Segment tree
+        FenwickTree<FenwickTree.Data.RangeSumData<Float>> tree = new FenwickTree<FenwickTree.Data.RangeSumData<Float>>(segments);
+
+        FenwickTree.Data.RangeSumData<Float> query = tree.query(0, 3);
+ 
+        query = tree.query(2);
+        assertTrue("Segment tree query error. query=2 result="+query, tree, query.sum.compareTo(new Float(6.11)) == 0);
+
+        query = tree.query(0, 1); // stabbing
+        assertTrue("Segment tree query error. query=0->1 result="+ query, tree, query.sum.compareTo(Float.sum(new Float(1.11),(new Float(2.11)))) == 0);
+    }
+
+
+    // Requirement: 
+    // Added branch coverage for when an element is null when using the functions combined and separate for every case 
+    // except the case in separate where "this.sum == null && data.sum != null" since it will not be possible to test as 
+    // a separated number supposed to be null (this.sum) from a sum which is not null (data.sum) is equal to 0.0 and not null 
+    // when separated
+
+    @Test
+    public void testNullFenwickTree() {
+        List<FenwickTree.Data.RangeSumData<Float>> segments = new ArrayList<FenwickTree.Data.RangeSumData<Float>>();
+        segments.add(new FenwickTree.Data.RangeSumData<Float>(0,  null));
+        segments.add(new FenwickTree.Data.RangeSumData<Float>(1,  null));
+        segments.add(new FenwickTree.Data.RangeSumData<Float>(2,  new Float(6.11)));
+        segments.add(new FenwickTree.Data.RangeSumData<Float>(3,  null));
+        //segments.add(new FenwickTree.Data.RangeSumData<Float>(2,  (Float) new Float(6.11)));
+        //segments.add(new FenwickTree.Data.RangeSumData<Float>(3,  (Float) new Float(7.11)));
+
+        // No matter which order the data is given, all tests should pass
+
+        // Initial order.
+        testNullFenwickTree(segments);
+
+        // Randomize it
+        Collections.shuffle(segments);
+        testNullFenwickTree(segments);
+
+        // Try in order
+        Collections.sort(segments);
+        testNullFenwickTree(segments);
+
+        // Try reverse order
+        Collections.sort(segments,REVERSE);
+        testNullFenwickTree(segments);
+    }
+
+     private void testNullFenwickTree(List<FenwickTree.Data.RangeSumData<Float>> segments) {   // Range Sum Segment tree
+        FenwickTree<FenwickTree.Data.RangeSumData<Float>> tree1 = new FenwickTree<FenwickTree.Data.RangeSumData<Float>>(segments);
+
+        FenwickTree.Data.RangeSumData<Float> query = tree1.query(0, 3);
+
+        query = tree1.query(1);
+        assertTrue("Segment tree query error. query=1 result="+query, tree1, query.sum == null);
+
+        query = tree1.query(2);
+        assertTrue("Segment tree query error. query=2 result="+query, tree1, query.sum.compareTo(new Float(6.11)) == 0);
+
+        query = tree1.query(3);
+        assertTrue("Segment tree query error. query=3 result="+query, tree1, query.sum == 0.0);
+
+        query = tree1.query(0, 1); // stabbing
+        assertTrue("Segment tree query error. query=0->1 result="+ query, tree1, query.sum == null);
+
+        query = tree1.query(0, 2); // stabbing
+        assertTrue("Segment tree query error. query=0->2 result="+ query, tree1, query.sum.compareTo(new Float(6.11)) == 0);
+
+        query = tree1.query(0, 3); // stabbing
+        assertTrue("Segment tree query error. query=0->3 result="+ query, tree1, query.sum.compareTo(new Float(6.11)) == 0);
+        
+        query = tree1.query(2, 3); // stabbing
+        assertTrue("Segment tree query error. query=2->3 result="+ query, tree1, query.sum.compareTo(new Float(6.11)) == 0);
+
+
+    }
+
+
+
+
 }
