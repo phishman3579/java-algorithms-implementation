9c8d68f2b2bb56a342072e7840203a08bfab9c98
Abhishek Ahuja <abhishekahuja35@gmail.com>
2022-12-02 17:17:43 -0500

    Added test cases to increase coverage
    Assignment: Added test cases to increase coverage

--- test/com/jwetherell/algorithms/data_structures/test/SuffixTrieTests.java
+++ test/com/jwetherell/algorithms/data_structures/test/SuffixTrieTests.java
@@ -1,6 +1,11 @@
 package com.jwetherell.algorithms.data_structures.test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+
+//importing two additional required utilities
+//Author: Abhishek Ahuja
+import java.util.Set;
+import java.util.TreeSet;
 
 import org.junit.Test;
 
@@ -15,4 +20,22 @@ public class SuffixTrieTests {
         SuffixTrie<String> trie = new SuffixTrie<String>(bookkeeper);
         assertTrue(SuffixTreeTest.suffixTreeTest(trie, bookkeeper));
     }
+    
+    //New Tests Added
+    //Author: Abhishek Ahuja
+    
+    @Test
+    public void testAddGet() {
+    	SuffixTrie<String> testTrie = new SuffixTrie<String>("batman");
+    	String partner = "robin";
+        assertTrue(testTrie.add(partner));
+        assertNotNull(testTrie.getSuffixes());
+    }
+    
+    @Test
+    public void testString() {
+    	SuffixTrie<String> testTrie2 = new SuffixTrie<String>("superman");
+        assertNotNull(testTrie2.toString());
+    }
+    
 }
--- test/com/jwetherell/algorithms/data_structures/test/TrieMapTests.java
+++ test/com/jwetherell/algorithms/data_structures/test/TrieMapTests.java
@@ -1,6 +1,8 @@
 package com.jwetherell.algorithms.data_structures.test;
 
-import static org.junit.Assert.assertTrue;
+//Modified to include all Assert methods directly
+//Author: Abhishek Ahuja
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
@@ -9,6 +11,10 @@ import com.jwetherell.algorithms.data_structures.test.common.JavaMapTest;
 import com.jwetherell.algorithms.data_structures.test.common.MapTest;
 import com.jwetherell.algorithms.data_structures.test.common.Utils;
 import com.jwetherell.algorithms.data_structures.test.common.Utils.TestData;
+
+//Imported
+//Author: Abhishek Ahuja
+import com.jwetherell.algorithms.data_structures.List;
 
 public class TrieMapTests {
 
@@ -25,4 +31,52 @@ public class TrieMapTests {
         assertTrue(JavaMapTest.testJavaMap(jMap, String.class, mapName,
                                            data.unsorted, data.sorted, data.invalid));
     }
+   
+    //Tests Added
+    //Author: Abhishek Ahuja
+    
+    @Test
+    public void testPutGet() {
+    	TrieMap<String,Integer> trieMap1 = new TrieMap<String,Integer>();
+    	TrieMap<String,Integer> trieMap2 = new TrieMap<String,Integer>();
+    	String key = "Green";
+        Integer value = 1;
+    	trieMap1.put(key,value);
+        trieMap2.put(key,value);
+        assertTrue(trieMap1.contains(key));
+        assertNotNull(trieMap1.get(key));
+        assertNull(trieMap1.get("Blue"));
+        assertEquals(trieMap1.toString(),trieMap2.toString());
+    	
+   }
+    
+    @Test
+    public void testClear() {
+    	
+    	List.ArrayList<Integer> newList = new List.ArrayList<Integer>();
+    	TrieMap<String,Integer> trieMap3 = new TrieMap<String,Integer>();
+        java.util.Map<String, Integer> jMap1 = trieMap3.toMap();
+        newList.add(1);
+        newList.clear();
+        trieMap3.clear();
+        jMap1.clear();
+        assertNotNull(jMap1.toString());
+        
+    }
+    
+    @Test
+    public void toStringTest() {        
+        StringBuilder builder = new StringBuilder();
+        builder.append("Build");
+    }
+    
+    @Test
+    public void testValidate() {
+        TrieMap<String,String> trieMap4 = new TrieMap<String,String>();
+        String key = "abc";
+        String val = null;
+        trieMap4.put(key, val);
+        assertTrue(!trieMap4.validate());
+    }
+    
 }
