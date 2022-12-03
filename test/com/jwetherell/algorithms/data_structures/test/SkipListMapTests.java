5add4871e5ba2b0584216de05c05e6fc9a30fc94
Maha Ali <43891334+MahaAli18@users.noreply.github.com>
2022-12-03 18:49:26 -0500

    Update SkipListMapTests.java

--- test/com/jwetherell/algorithms/data_structures/test/SkipListMapTests.java
+++ test/com/jwetherell/algorithms/data_structures/test/SkipListMapTests.java
@@ -1,9 +1,11 @@
 package com.jwetherell.algorithms.data_structures.test;
 
-import static org.junit.Assert.assertTrue;
+
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.jwetherell.algorithms.data_structures.List;
 import com.jwetherell.algorithms.data_structures.SkipListMap;
 import com.jwetherell.algorithms.data_structures.test.common.JavaMapTest;
 import com.jwetherell.algorithms.data_structures.test.common.MapTest;
@@ -11,19 +13,80 @@ import com.jwetherell.algorithms.data_structures.test.common.Utils;
 import com.jwetherell.algorithms.data_structures.test.common.Utils.TestData;
 
 public class SkipListMapTests {
+	 SkipListMap<String, Integer> map = new SkipListMap<String, Integer>();
 
     @Test
     public void testSkipListMap() {
         TestData data = Utils.generateTestData(1000);
 
         String mapName = "SkipListMap";
-        SkipListMap<String,Integer> map = new SkipListMap<String,Integer>();
-        java.util.Map<String,Integer> jMap = map.toMap();
+        java.util.Map<String, Integer> jMap = map.toMap();
 
         assertTrue(MapTest.testMap(map, String.class, mapName,
-                                   data.unsorted, data.invalid));
+                data.unsorted, data.invalid));
         assertTrue(JavaMapTest.testJavaMap(jMap, Integer.class, mapName,
-                                           data.unsorted, data.sorted, data.invalid));
+                data.unsorted, data.sorted, data.invalid));
+        
+        
+       
     }
+    
+    @Test
+    public void getTest() {
+    	 Integer value = 1;
+    	 String key = "SkipListMap";
+    	 SkipListMap<String, Integer> expected = new SkipListMap<String, Integer>();
+    	 expected.put(key, value);
+    	 map.put(key, value);
+         assertTrue(map.contains(key)); 
+         assertTrue(map.get(key)!= null);
+         assertNull(map.get("NotKey"));
+    	 }
+    
+    @Test
+    public void clearListTest() {
+    	 List.ArrayList<Integer> mapList = new List.ArrayList<Integer>();
+         java.util.Map<String, Integer> jMap = map.toMap();
+
+         mapList.add(201);
+         mapList.clear();
+         map.clear();
+         jMap.clear();
+    }
+    
+    @Test
+    public void validate() {
+    	List.ArrayList<Integer> mapList = new List.ArrayList<Integer>();
+        String test = "Work";
+        java.util.Map<String, Integer> jMap = map.toMap();
+        StringBuilder builder = new StringBuilder();
+        builder.append(test);
+        assertNotNull(jMap.toString()); 
+        assertNotNull(map.toString()); 
+        
+        
+    }
+    
+    @Test
+    public void toStringtest() {
+
+        map.put("map", 1234)
+        String expected= "1234";
+
+        assertNotNull(map.toString());
+
+    }
+    
+    @Test
+
+    public void testValidate() {
+        SkipListMap<String, String> mapTest = new SkipListMap<String, String>();
+        String key = "book";
+        String val = null;
+        mapTest.put(key, val);
+        assertTrue(!mapTest.validate());
+
+    }
+
 
 }
