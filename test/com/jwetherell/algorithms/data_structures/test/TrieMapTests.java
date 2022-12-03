a8327d09a5c5d7f59265624c36853403dcb03163
Harshdeep1402 <46749525+Harshdeep1402@users.noreply.github.com>
2022-12-03 03:13:00 -0500

    Update TrieMapTests.java

--- test/com/jwetherell/algorithms/data_structures/test/TrieMapTests.java
+++ test/com/jwetherell/algorithms/data_structures/test/TrieMapTests.java
@@ -25,4 +25,46 @@ public class TrieMapTests {
         assertTrue(JavaMapTest.testJavaMap(jMap, String.class, mapName,
                                            data.unsorted, data.sorted, data.invalid));
     }
+    @Test
+	public void testclear() {
+		TrieMap<String, Integer> map = new TrieMap<String, Integer>();
+		java.util.Map<String, Integer> map1 = map.toMap();
+		List.ArrayList<Integer> mapList = new List.ArrayList<Integer>();
+		mapList.clear();
+		map1.clear();
+
+	}
+
+	@Test
+	public void testget() {
+		TrieMap<String, String> map = new TrieMap<String, String>();
+		String key = "abc";
+		String value = "val";
+		map.put(key, value);
+
+		assertTrue(map.get(key) != null);
+		assertTrue(map.get("null") == null);
+	}
+
+	@Test
+	public void testValidate() {
+		TrieMap<String, String> map_test = new TrieMap<String, String>();
+		String key = "abc";
+		String val = null;
+		map_test.put(key, val);
+
+		assertTrue(!map_test.validate());
+	}
+
+	@Test
+	public void testToString() {
+		TrieMap<String, String> map = new TrieMap<String, String>();
+		java.util.Map<String, String> map1 = map.toMap();
+		map.put("Map", "val1");
+		map1.put("Map", "val2");
+
+		assertEquals(map.toString(), map.toString());
+		assertNotNull(map1.toString());
+
+	}
 }
