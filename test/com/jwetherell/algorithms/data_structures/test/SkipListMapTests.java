e13a931c06ba61bc6a10423e249b440f17edd305
Harshdeep1402 <46749525+Harshdeep1402@users.noreply.github.com>
2022-12-03 03:14:12 -0500

    Update SkipListMapTests.java

--- test/com/jwetherell/algorithms/data_structures/test/SkipListMapTests.java
+++ test/com/jwetherell/algorithms/data_structures/test/SkipListMapTests.java
@@ -25,5 +25,61 @@ public class SkipListMapTests {
         assertTrue(JavaMapTest.testJavaMap(jMap, Integer.class, mapName,
                                            data.unsorted, data.sorted, data.invalid));
     }
+    
+    @Test
+        public void testGet() {
+            SkipListMap<String, Integer> map = new SkipListMap<String, Integer>();
+            Integer value = 1;
+            String key = "SkipListMap";
+            SkipListMap<String, Integer> expected = new SkipListMap<String, Integer>();
+            expected.put(key, value);
+            map.put(key, value);
 
+            assertTrue(map.contains(key));
+            assertTrue(map.get(key) != null);
+            assertNull(map.get("NotKey"));
+        }
+
+    @Test
+    public void testClearList() {
+        SkipListMap<String, Integer> map = new SkipListMap<String, Integer>();
+        List.ArrayList<Integer> mapList = new List.ArrayList<Integer>();
+        java.util.Map<String, Integer> jMap = map.toMap();
+        mapList.add(201);
+        mapList.clear();
+        map.clear();
+        jMap.clear();
+    }
+
+    @Test
+    public void testToMap() {
+        SkipListMap<String, Integer> mapTest = new SkipListMap<String, Integer>();
+        java.util.Map<String, Integer> jMap = mapTest.toMap();
+
+        assertNotNull(jMap.toString());
+        assertNotNull(mapTest.toString());
+    }
+
+    @Test
+    public void testToString() {
+        SkipListMap<String, Integer> mapTest = new SkipListMap<String, Integer>();
+        mapTest.put("abc", 1234);
+        String actual = mapTest.toString();
+        String expected = "abc=1234";
+
+        assertNotNull(mapTest.toString());
+        assertTrue(expected.equals(actual));
+    }
+
+    @Test
+    public void testValidate() {
+        SkipListMap<String, String> mapTest = new SkipListMap<String, String>();
+        String key = "abc";
+        String val = null;
+        mapTest.put(key, val);
+        mapTest.validate();
+
+        assertTrue(!mapTest.validate());
+
+        }
 }
