4225852650c78b9367a467244050b35de3060d7c
gargi29 <gargi28111996@gmail.com>
2022-12-03 23:23:11 -0500

    gargi pull request sysc 5807

--- test/com/jwetherell/algorithms/data_structures/test/SuffixTrieTests.java
+++ test/com/jwetherell/algorithms/data_structures/test/SuffixTrieTests.java
@@ -15,4 +15,40 @@ public class SuffixTrieTests {
         SuffixTrie<String> trie = new SuffixTrie<String>(bookkeeper);
         assertTrue(SuffixTreeTest.suffixTreeTest(trie, bookkeeper));
     }
+    @Test
+    public void testEmptyTree() {
+        //Given
+        String emptyStr = "";
+        //When
+        SuffixTrie<String> actualTrie = new SuffixTrie<String>(emptyStr);
+        //Then
+        assertEquals("Tree has no nodes.",actualTrie.toString());
+    }
+
+
+    @Test
+    public void testSubSequence() {
+        //When
+        SuffixTrie<String> actualTrie = new SuffixTrie<String>("Potterhead");
+        //Then
+        assertTrue(actualTrie.doesSubStringExist("head"));
+        assertFalse(actualTrie.doesSubStringExist("muggle"));
+    }
+
+    @Test
+    public void testCountNodes() {
+        //Given
+        int leftLimit = 97;
+        int rightLimit = 122;
+        int length = 12;
+        Random random = new Random();
+        String str = random.ints(leftLimit, rightLimit+1)
+                .limit(length)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString();
+        //When
+        SuffixTrie<String> actualTrie = new SuffixTrie<String>(str);
+        //Then
+        System.out.println(actualTrie);
+        assertEquals(str.length(), actualTrie.getSuffixes().size());
+    }
 }
