eecff4b998b574c84f79307c2717ea0a57592c3d
Harshil Maniar <maniarharshil1000@gmail.com>
2022-12-03 23:56:04 -0500

    added test cases for Matrix, DisjointSet, SuffixTrie, SkipListMap

--- test/com/jwetherell/algorithms/data_structures/test/DisjointSetTests.java
+++ test/com/jwetherell/algorithms/data_structures/test/DisjointSetTests.java
@@ -14,26 +14,26 @@ public class DisjointSetTests {
     public void testDisjointSet1() {
         final int max = 10;
         final int[] array = new int[max];
-        for (int i=0; i<array.length; i++)
-            array[i] = i+1;
+        for (int i = 0; i < array.length; i++)
+            array[i] = i + 1;
 
         final DisjointSet.Item<Integer>[] items = new DisjointSet.Item[array.length];
-        for (int i=0; i<items.length; i++) {
+        for (int i = 0; i < items.length; i++) {
             final int v = array[i];
             final DisjointSet.Item<Integer> s = DisjointSet.makeSet(v);
             items[i] = s;
             final DisjointSet.Item<Integer> f = DisjointSet.find(s);
-            Assert.assertTrue(f!=null);
+            Assert.assertTrue(f != null);
             Assert.assertTrue(f.getValue() == v);
         }
         if (DEBUG)
             System.out.println(toString(items));
 
-        final int half = items.length/2;
+        final int half = items.length / 2;
 
         // first half set
         DisjointSet.Item<Integer> first = items[0];
-        for (int i=1; i<half; i++) {
+        for (int i = 1; i < half; i++) {
             final DisjointSet.Item<Integer> item = items[i];
             first = DisjointSet.union(first, item);
         }
@@ -41,7 +41,7 @@ public class DisjointSetTests {
             System.out.println(toString(items));
         // second half set
         DisjointSet.Item<Integer> second = items[half];
-        for (int i=half+1; i<items.length; i++) {
+        for (int i = half + 1; i < items.length; i++) {
             final DisjointSet.Item<Integer> item = items[i];
             second = DisjointSet.union(second, item);
         }
@@ -49,26 +49,28 @@ public class DisjointSetTests {
             System.out.println(toString(items));
 
         // Make sure all items in first set are actually in first set and vice versa
-        for (int i=0; i<half; i++) {
+        for (int i = 0; i < half; i++) {
             final DisjointSet.Item<Integer> item = items[i];
             final DisjointSet.Item<Integer> result = DisjointSet.find(item);
             Assert.assertTrue(result.equals(first));
+            Assert.assertFalse(result.equals(i));
         }
-        for (int i=half; i<items.length; i++) {
+        for (int i = half; i < items.length; i++) {
             final DisjointSet.Item<Integer> item = items[i];
             final DisjointSet.Item<Integer> result = DisjointSet.find(item);
             Assert.assertTrue(result.equals(second));
         }
 
-        // union the second set and the first set and make sure the superset is created properly
+        // union the second set and the first set and make sure the superset is created
+        // properly
         DisjointSet.Item<Integer> superSet = first;
-        for (int i=0; i<items.length; i++) {
+        for (int i = 0; i < items.length; i++) {
             final DisjointSet.Item<Integer> item = items[i];
             superSet = DisjointSet.union(superSet, item);
         }
         if (DEBUG)
             System.out.println(toString(items));
-        for (int i=0; i<items.length; i++) {
+        for (int i = 0; i < items.length; i++) {
             final DisjointSet.Item<Integer> item = items[i];
             final DisjointSet.Item<Integer> result = DisjointSet.find(item);
             Assert.assertTrue(result.equals(superSet));
@@ -79,40 +81,40 @@ public class DisjointSetTests {
     public void testDisjointSet2() {
         final int max = 10;
         final int[] array = new int[max];
-        for (int i=0; i<array.length; i++)
-            array[i] = i+1;
+        for (int i = 0; i < array.length; i++)
+            array[i] = i + 1;
 
         final DisjointSet.Item<Integer>[] items = new DisjointSet.Item[array.length];
-        for (int i=0; i<items.length; i++) {
+        for (int i = 0; i < items.length; i++) {
             final int v = array[i];
             final DisjointSet.Item<Integer> s = DisjointSet.makeSet(v);
             items[i] = s;
             final DisjointSet.Item<Integer> f = DisjointSet.find(s);
-            Assert.assertTrue(f!=null);
+            Assert.assertTrue(f != null);
             Assert.assertTrue(f.getValue() == v);
         }
         if (DEBUG)
             System.out.println(toString(items));
 
-        final int quarter = items.length/4;
+        final int quarter = items.length / 4;
 
         DisjointSet.Item<Integer> i1 = items[0];
-        for (int i=1; i<quarter; i++) {
+        for (int i = 1; i < quarter; i++) {
             final DisjointSet.Item<Integer> item = items[i];
             i1 = DisjointSet.union(i1, item);
         }
         DisjointSet.Item<Integer> i2 = items[quarter];
-        for (int i=quarter+1; i<2*quarter; i++) {
+        for (int i = quarter + 1; i < 2 * quarter; i++) {
             final DisjointSet.Item<Integer> item = items[i];
             i2 = DisjointSet.union(i2, item);
         }
-        DisjointSet.Item<Integer> i3 = items[2*quarter];
-        for (int i=2*quarter+1; i<3*quarter; i++) {
+        DisjointSet.Item<Integer> i3 = items[2 * quarter];
+        for (int i = 2 * quarter + 1; i < 3 * quarter; i++) {
             final DisjointSet.Item<Integer> item = items[i];
             i3 = DisjointSet.union(i3, item);
         }
-        DisjointSet.Item<Integer> i4 = items[3*quarter];
-        for (int i=3*quarter+1; i<array.length; i++) {
+        DisjointSet.Item<Integer> i4 = items[3 * quarter];
+        for (int i = 3 * quarter + 1; i < array.length; i++) {
             final DisjointSet.Item<Integer> item = items[i];
             i4 = DisjointSet.union(i4, item);
         }
@@ -122,7 +124,7 @@ public class DisjointSetTests {
         DisjointSet.Item<Integer> s1 = DisjointSet.union(i1, i2);
         DisjointSet.Item<Integer> s2 = DisjointSet.union(i3, i4);
         DisjointSet.Item<Integer> s3 = DisjointSet.union(s1, s2);
-        Assert.assertTrue(s3.getValue()==1 && s3.getRank()==3);
+        Assert.assertTrue(s3.getValue() == 1 && s3.getRank() == 3);
         if (DEBUG)
             System.out.println(toString(items));
     }
@@ -136,4 +138,43 @@ public class DisjointSetTests {
         builder.append("}\n");
         return builder.toString();
     }
+
+    @Test
+    public void testDisjointSet3() {
+        final int max = 10;
+        final int[] array = new int[max];
+        for (int i = 0; i < array.length; i++)
+            array[i] = i + 1;
+
+        final DisjointSet.Item<Integer>[] items = new DisjointSet.Item[array.length];
+        for (int i = 0; i < items.length; i++) {
+            final int v = array[i];
+            final DisjointSet.Item<Integer> s = DisjointSet.makeSet(v);
+            items[i] = s;
+            if (i == 7) {
+                final DisjointSet.Item<Integer> f = DisjointSet.find(null);
+                Assert.assertNull(f);
+            } else {
+                final DisjointSet.Item<Integer> f = DisjointSet.find(s);
+                Assert.assertTrue(f != null);
+                Assert.assertTrue(f.getValue() == v);
+            }
+        }
+
+        DisjointSet.Item<Integer> i1 = items[0];
+        DisjointSet.Item<Integer> item = items[1];
+        DisjointSet.Item<Integer> i2 = items[2];
+        DisjointSet.Item<Integer> i3 = items[3];
+        i1 = DisjointSet.union(i1, item);
+        System.out.println(i1);
+        System.out.println(i2);
+        i1 = DisjointSet.union(i1, i2);
+        i1 = DisjointSet.union(i3, i1);
+        DisjointSet.union(null, item);
+        DisjointSet.union(item, null);
+        DisjointSet.union(item, item);
+        DisjointSet.find(null);
+        DisjointSet.union(null, null);
+    }
+
 }
--- test/com/jwetherell/algorithms/data_structures/test/SkipListMapTests.java
+++ test/com/jwetherell/algorithms/data_structures/test/SkipListMapTests.java
@@ -1,14 +1,15 @@
 package com.jwetherell.algorithms.data_structures.test;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 import com.jwetherell.algorithms.data_structures.SkipListMap;
 import com.jwetherell.algorithms.data_structures.test.common.JavaMapTest;
 import com.jwetherell.algorithms.data_structures.test.common.MapTest;
 import com.jwetherell.algorithms.data_structures.test.common.Utils;
 import com.jwetherell.algorithms.data_structures.test.common.Utils.TestData;
+
+import com.jwetherell.algorithms.data_structures.List;
 
 public class SkipListMapTests {
 
@@ -17,13 +18,56 @@ public class SkipListMapTests {
         TestData data = Utils.generateTestData(1000);
 
         String mapName = "SkipListMap";
-        SkipListMap<String,Integer> map = new SkipListMap<String,Integer>();
-        java.util.Map<String,Integer> jMap = map.toMap();
+        SkipListMap<String, Integer> map = new SkipListMap<String, Integer>();
+        java.util.Map<String, Integer> jMap = map.toMap();
 
         assertTrue(MapTest.testMap(map, String.class, mapName,
-                                   data.unsorted, data.invalid));
+                data.unsorted, data.invalid));
         assertTrue(JavaMapTest.testJavaMap(jMap, Integer.class, mapName,
-                                           data.unsorted, data.sorted, data.invalid));
+                data.unsorted, data.sorted, data.invalid));
+    }
+
+    @Test
+    public void putAndGet() {
+        SkipListMap<String, Integer> map1 = new SkipListMap<String, Integer>();
+        map1.put("TestString", 1);
+        assertTrue(map1.contains("TestString"));
+        assertNotNull(map1.get("TestString"));
+        assertNull(map1.get("NotPresent"));
+        assertEquals("TestString=1", map1.toString());
+    }
+
+    @Test
+    public void listTest() {
+        List.ArrayList<Integer> arr = new List.ArrayList<Integer>();
+        SkipListMap<String, Integer> skipList = new SkipListMap<String, Integer>();
+        java.util.Map<String, Integer> map = skipList.toMap();
+        arr.add(1);
+        arr.clear();
+        skipList.clear();
+        map.clear();
+        map.put("map", 1);
+        map.put("map", 2);
+        assertNotNull(skipList.toString());
+        assertNotNull(map.toString());
+    }
+
+    @Test
+    public void stringBuilder() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Build");
+    }
+
+    @Test
+    public void validateTest() {
+        SkipListMap<String, Integer> skipListMap1 = new SkipListMap<String, Integer>();
+        SkipListMap<String, Integer> skipListMap2 = new SkipListMap<String, Integer>();
+        skipListMap1.put("validate", null);
+        // System.out.println(skipListMap2.toString());
+        assertFalse(skipListMap1.validate());
+        assertTrue(skipListMap2.validate());
+        ;
+
     }
 
 }
