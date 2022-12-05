2b5fa8e101dd130f56278f224b9c34ac04584cc9
AbhijeetJadeja <118860153+AbhijeetJadeja@users.noreply.github.com>
2022-12-04 19:53:48 -0500

    Add files via upload

---
+++ SuffixTrieTests.java
@@ -0,0 +1,32 @@
+package com.jwetherell.algorithms.data_structures.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.jwetherell.algorithms.data_structures.SuffixTrie;
+import com.jwetherell.algorithms.data_structures.test.common.SuffixTreeTest;
+
+public class SuffixTrieTests {
+
+    @Test
+    public void testSuffixTrie() {
+        String bookkeeper = "bookkeeper";
+        SuffixTrie<String> trie = new SuffixTrie<String>(bookkeeper);
+        assertTrue(SuffixTreeTest.suffixTreeTest(trie, bookkeeper));
+    }
+
+    @Test
+    public void testAdd() {
+    	SuffixTrie<String> suffixTrieTest = new SuffixTrie<String>("book");
+    	String part = "pen";
+        assertTrue(suffixTrieTest.add(part));
+        assertNotNull(suffixTrieTest.getSuffixes());
+    }
+
+    @Test
+    public void testString() {
+    	SuffixTrie<String> suffixTrieTest2 = new SuffixTrie<String>("paper");
+        assertNotNull(suffixTrieTest2.toString());
+    }
+}
